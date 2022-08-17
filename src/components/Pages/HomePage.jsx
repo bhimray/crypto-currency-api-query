40382259cf60479f77c6824d22141c063d55bddf
Bilen <raybimlendra@gmail.com>
2022-08-17 18:32:09 +0545

    new file:   dist/output.css 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	new file:   src/components/Pages/HomePage.css 	modified:   src/components/Pages/HomePage.jsx 	modified:   src/index.css 	new file:   tailwind.config.js

--- src/components/Pages/HomePage.jsx
+++ src/components/Pages/HomePage.jsx
@@ -1,22 +1,27 @@
 import React from 'react'
 import {DataContext} from '../DataQuery/dataQuery'
 import { useContext } from 'react'
-
+import "./HomePage.css"
 const HomePage = (props) => {
   const {coinRanking} = useContext(DataContext)
   console.log(coinRanking, "is in the homepage")
   return (
     <div>
-      {
-      Object.entries(coinRanking).map(([key, value], i)=>{
-        console.log("this is value");
-        return(
-          <>
-            <p key={i}>{key}--{value}</p>
-          </>
-        )
-      })
-      }
+      <div className="summaryContainer">
+        {
+        Object.entries(coinRanking).map(([key, value], i)=>{
+          console.log("this is value");
+          return(
+            <>
+            <div className='coinSummary'>
+              <span key={i}>{key}</span>
+              <span key={i}>{value}</span>
+            </div>
+            </>
+          )
+        })
+        }
+      </div>
     </div>
   )
 }
